04f426786baf17de1ea7aa47730a7e80cc679738
wei19891207 <2812035930@qq.com>
2022-04-30 15:04:00 +0800

    实现了简单的登录和注册功能

---
+++ src/main/java/cn/wzx/Login.java
@@ -0,0 +1,160 @@
+package cn.wzx;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+/*
+ * Created by JFormDesigner on Sat Apr 02 15:41:52 CST 2022
+ */
+
+
+/**
+ * @author 1
+ */
+public class Login extends JFrame {
+    public Login() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        label1 = new JLabel();
+        textField1 = new JTextField("guet");
+        label2 = new JLabel();
+        textField2 = new JTextField("guet1234");
+        button1 = new JButton();
+        button2 = new JButton();
+        radioButton1 = new JRadioButton();
+        radioButton2 = new JRadioButton();
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //---- label1 ----
+        label1.setText("\u7528\u6237\u540d\uff1a");
+        contentPane.add(label1);
+        label1.setBounds(new Rectangle(new Point(115, 90), label1.getPreferredSize()));
+        contentPane.add(textField1);
+        textField1.setBounds(185, 85, 100, textField1.getPreferredSize().height);
+
+        //---- label2 ----
+        label2.setText("\u5bc6\u7801\uff1a");
+        contentPane.add(label2);
+        label2.setBounds(new Rectangle(new Point(120, 125), label2.getPreferredSize()));
+        contentPane.add(textField2);
+        textField2.setBounds(185, 120, 100, textField2.getPreferredSize().height);
+
+        //---- radioButton1 ----
+
+        radioButton1.setText("\u7528\u6237");
+        contentPane.add(radioButton1);
+        radioButton1.setBounds(new Rectangle(new Point(140, 160), radioButton1.getPreferredSize()));
+
+        //---- radioButton2 ----
+        radioButton2.setText("商家");
+        contentPane.add(radioButton2);
+        radioButton2.setBounds(new Rectangle(new Point(210, 160), radioButton2.getPreferredSize()));
+
+
+
+        //---- button1 ----
+        button1.addActionListener(
+                (e) -> {
+                    /*
+                    1、拿到登录界面的用户名和密码
+                    2、去和数据库中的用户名和密码比对
+                    3、创建了数据库和表
+                    4、添加一条记录
+                    INSERT INTO sys_user (id,name,password) VALUES(1111,'guet','guet1234');
+                     */
+                    String username = textField1.getText();
+                    String password = textField2.getText();
+
+                    String user = "root";
+                    String dbPassword = "123456";
+                    String url = "jdbc:mysql://localhost:3306/teashop?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+                    Connection conn = null;
+                    // ?sql?????????????
+                    String sql = "SELECT * FROM sys_user WHERE name='" + username + "' AND password='" + password + "'";
+                    System.out.println(sql);
+                    ResultSet rs = null;//结果集：内存，存储了查询到的数据；内存区有一个游标，执行完查询的时候，不指向任何记录
+                    Statement stmt = null;//语句对象，容易产生注入攻击
+
+                    try {
+                        conn = DriverManager.getConnection(url, user, dbPassword);
+
+                        stmt = conn.createStatement();
+                        rs = stmt.executeQuery(sql);
+                        if (rs.next()) {//让游标向下移动一次
+
+                            /*
+                            身份选择功能
+                             */
+                        if(radioButton1.isSelected()){
+
+                            Main main=new Main();
+                            main.setVisible(true);
+                            System.out.println("登陆成功");
+                            this.setVisible(false);
+                        }
+                        else if(radioButton2.isSelected()){
+                            Password pwd=new Password();
+                            pwd.setVisible(true);
+                        }
+
+
+                        } else {
+                            System.out.println("用户名或密码错误");
+                        }
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+        );
+        button1.setText("\u767b\u5f55");
+        contentPane.add(button1);
+        button1.setBounds(new Rectangle(new Point(125, 200), button1.getPreferredSize()));
+
+        //button2
+        button2.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Register register=new Register();
+                        register.setVisible(true);
+
+                    }
+                }
+        );
+        button2.setText("注册");
+        contentPane.add(button2);
+        button2.setBounds(new Rectangle(new Point(230, 200), button1.getPreferredSize()));
+
+
+
+        contentPane.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setLocationRelativeTo(getOwner());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);//组件设置为可见
+    }
+
+    private JLabel label1;
+    private JTextField textField1;
+    private JLabel label2;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
