1689c2c4950ffed2bf75e28850fe56c2997dc184
MMMMintM <1962849542@qq.com>
2022-05-06 00:02:46 +0800

    完成了日销售额跟随订单变化而变化的功能

--- src/main/java/cn/edu/guet/zt/statistics/dao/DailySalesDao.java
+++ src/main/java/cn/edu/guet/zt/statistics/dao/DailySalesDao.java
@@ -1,12 +1,17 @@
 package cn.edu.guet.zt.statistics.dao;
 
+import cn.edu.guet.zt.statistics.bean.OrderInfo;
 import cn.edu.guet.zt.statistics.bean.SalesVolume;
+import java.util.Date;
+
+import javax.xml.crypto.Data;
+import java.sql.SQLException;
 
 public interface DailySalesDao {
     /**
      * 此接口提供表daily_Sales跟随表sales_volum变动而变动的方法
      */
-    void updateDailySales(SalesVolume salesVolume);
+    void updateDailySales(OrderInfo orderInfo, Date date) throws SQLException;
 
 
 }
--- src/main/java/cn/edu/guet/zt/statistics/dao/impl/DailySalesDaoImpl.java
+++ src/main/java/cn/edu/guet/zt/statistics/dao/impl/DailySalesDaoImpl.java
@@ -1,11 +1,59 @@
 package cn.edu.guet.zt.statistics.dao.impl;
 
+import cn.edu.guet.hld.util.ConnectionHander;
+import cn.edu.guet.zt.statistics.bean.OrderInfo;
 import cn.edu.guet.zt.statistics.bean.SalesVolume;
 import cn.edu.guet.zt.statistics.dao.DailySalesDao;
+import cn.edu.guet.zt.statistics.util.SalesConnectionHandler;
+
+import java.sql.*;
+import java.util.Date;
+
+import java.util.Date;
 
 public class DailySalesDaoImpl implements DailySalesDao {
     @Override
-    public void updateDailySales(SalesVolume salesVolume) {
+    public void updateDailySales(OrderInfo orderInfo, Date date) throws SQLException{
+        Connection conn = null;
+        ResultSet rs = null;
+        Statement stmt = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn  = ConnectionHander.getConnection();
+
+            stmt = conn.createStatement();
+            String nowDate = date.toString();
+
+            String sql_1 = "SELECT * FROM daily_sales WHERE day = '" + nowDate + "'";
+
+            rs = stmt.executeQuery(sql_1);
+
+            if(!rs.next()){
+                String sql = "INSERT INTO daily_sales VALUES (?,?)";
+                pstmt = conn.prepareStatement(sql);
+
+                pstmt.setString(1,orderInfo.getDate().toString());
+                pstmt.setInt(2,orderInfo.getOrder_price());
+
+                pstmt.executeUpdate();
+            }else {
+                String sql = "UPDATE daily_sales SET sales = Sales + ? WHERE date = ?;";
+
+                pstmt = conn.prepareStatement(sql);
+
+                pstmt.setInt(1,orderInfo.getOrder_price());
+                pstmt.setString(2,orderInfo.getDate().toString());
+
+                pstmt.executeUpdate();
+            }
+
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new SQLException();
+        }
 
     }
 }
